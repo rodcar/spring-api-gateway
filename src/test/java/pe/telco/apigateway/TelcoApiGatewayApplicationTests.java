c998a5d2e20f19526b668c7f84f9a7767704d394
rodcar <ivanrod12@hotmail.com>
2023-01-07 23:30:20 -0500

    commit inicial

---
+++ src/test/java/pe/telco/apigateway/TelcoApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.telco.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelcoApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
